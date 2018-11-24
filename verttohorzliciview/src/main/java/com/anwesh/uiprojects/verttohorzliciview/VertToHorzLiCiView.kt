adefcb9990903ef5300abdf9751ec937dd51ed25
Anwesh43 <anweshthecool0@gmail.com>
2018-11-24 18:42:09 +0530

    adding drawVTHNode canvas extension method

--- verttohorzliciview/src/main/java/com/anwesh/uiprojects/verttohorzliciview/VertToHorzLiCiView.kt
+++ verttohorzliciview/src/main/java/com/anwesh/uiprojects/verttohorzliciview/VertToHorzLiCiView.kt
@@ -11,6 +11,7 @@ import android.view.MotionEvent
 import android.graphics.Color
 import android.app.Activity
 import android.content.Context
+import android.graphics.Path
 
 val nodes : Int = 5
 val lines : Int = 5
@@ -30,3 +31,34 @@ fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - factorValue()) * a.getInv
 
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = dir * scGap * mirrorValue(a, b)
 
+fun Canvas.drawVTHNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.color = color
+    paint.strokeCap = Paint.Cap.ROUND
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    save()
+    translate(gap * (i + 1), h / 2)
+    rotate(90f * sc2)
+    val path : Path = Path()
+    path.addCircle(0f, 0f, size, Path.Direction.CW)
+    clipPath(path)
+    drawPath(path, paint)
+    val yGap : Float = (2 * size) / (lines + 2)
+    var y : Float = size - yGap
+    drawLine(-size, y, size, y, paint)
+    for (j in 0..(lines - 1)) {
+        val sc : Float = sc1.divideScale(j, lines)
+        y -= yGap * sc
+        save()
+        translate(0f, y)
+        drawLine(-size, 0f, size, 0f, paint)
+        restore()
+    }
+    restore()
+}
