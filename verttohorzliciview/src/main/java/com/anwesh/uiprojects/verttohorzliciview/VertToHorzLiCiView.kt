fd58f367e5bdb72985a68f322ff9ff669685882b
Anwesh43 <anweshthecool0@gmail.com>
2018-11-24 18:29:21 +0530

    adding Float and Int extension methods

--- verttohorzliciview/src/main/java/com/anwesh/uiprojects/verttohorzliciview/VertToHorzLiCiView.kt
+++ verttohorzliciview/src/main/java/com/anwesh/uiprojects/verttohorzliciview/VertToHorzLiCiView.kt
@@ -13,15 +13,20 @@ import android.app.Activity
 import android.content.Context
 
 val nodes : Int = 5
-
 val lines : Int = 5
-
 val color : Int = Color.parseColor("#4A148C")
-
 val sizeFactor : Int = 3
-
 val strokeFactor : Int = 60
-
 val scDiv : Double = 0.51
-
 val scGap : Float = 0.05f
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(0f, this - i * n.getInverse())) * n
+
+fun Float.factorValue() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - factorValue()) * a.getInverse() + b.getInverse() * factorValue()
+
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = dir * scGap * mirrorValue(a, b)
+
