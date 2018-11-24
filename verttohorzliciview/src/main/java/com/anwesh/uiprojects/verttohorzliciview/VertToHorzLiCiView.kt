36f28e9b1d90d1a5a0709f3df9322277295a53e3
Anwesh43 <anweshthecool0@gmail.com>
2018-11-24 18:24:52 +0530

    adding libraries and defining constants

--- verttohorzliciview/src/main/java/com/anwesh/uiprojects/verttohorzliciview/VertToHorzLiCiView.kt
+++ verttohorzliciview/src/main/java/com/anwesh/uiprojects/verttohorzliciview/VertToHorzLiCiView.kt
@@ -3,3 +3,25 @@ package com.anwesh.uiprojects.verttohorzliciview
 /**
  * Created by anweshmishra on 24/11/18.
  */
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+
+val lines : Int = 5
+
+val color : Int = Color.parseColor("#4A148C")
+
+val sizeFactor : Int = 3
+
+val strokeFactor : Int = 60
+
+val scDiv : Double = 0.51
+
+val scGap : Float = 0.05f
