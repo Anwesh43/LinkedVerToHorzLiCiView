d046a2aab3698d2f417391c12f1b3cb076abd856
Anwesh43 <anweshthecool0@gmail.com>
2018-11-24 19:53:19 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedverttohorzliciview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedverttohorzliciview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedverttohorzliciview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.verttohorzliciview.VertToHorzLiCiView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        VertToHorzLiCiView.create(this)
     }
 }
